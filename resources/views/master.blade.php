fdeda28e0bd07b41d6cdd3dca2852e9d7a7c6626
Frank1290 <joshifranklin@gmail.com>
2020-03-18 18:18:40 -0400

    my vy first commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title> User Custom Registration and Login with Session </title>
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="../bootstrap/bootstrap.min.css">
+</head>
+
+<body>
+
+  @yield('content')
+
+
+</body>
+
+</html>
